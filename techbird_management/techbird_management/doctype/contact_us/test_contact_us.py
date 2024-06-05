afaef086ee693689d4ca5568c5b7a7e65b58edd5
suraj787 <test@example.com>
2024-06-05 07:37:55 +0000

    initial commit: Techbird Management

---
+++ techbird_management/techbird_management/doctype/contact_us/test_contact_us.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Tech bird and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestContactUs(FrappeTestCase):
+	pass
